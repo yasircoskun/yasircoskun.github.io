329a2a4bff72785db888329a9510010243d5f317
yasircoskun <yasir.coskun@outlook.com>
2022-05-27 02:06:25 +0300

    ChangeBackground connected to SubReddit via RSS

--- apps/ChangeBackground/script.js
+++ apps/ChangeBackground/script.js
@@ -1,3 +1,14 @@
+var wallappers = {
+    "Desktop": [
+        "https://yasircoskun.github.io/assets/images/wallpapers/desktop/SteinsGate.gif",
+        "https://orig00.deviantart.net/9f97/f/2017/100/5/8/vassar_assets_1_by_grenadekitten-db5d8ta.gif",
+    ],
+    "Mobile": [
+        "https://66.media.tumblr.com/95b2ba03ec0a1a2753def103f98b978c/tumblr_pqbvczetDg1t8nuqh_1280.gif",
+        "https://78.media.tumblr.com/80626c02ca522a84125574d137f5b412/tumblr_o9zxrhyPj21tgo74ho1_1280.gif",
+    ]
+}
+
 function changeBackgroundCookie(value) {
     setCookie('backgroundUrl', value, 1);
     setCookie('command', 'reload', 1);
@@ -30,3 +41,34 @@ function getCookie(cname) {
     }
     return "";
 }
+
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl.indexOf('?') != -1 ? theUrl + '&nocache=' + new Date().getTime() : theUrl + '?nocache=' + new Date().getTime(), false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+var rss_content = JSON.parse(httpGet("https://api.allorigins.win/get?url=https://www.reddit.com/r/Animewallpaper/search.rss?q=flair_name%3A%22Desktop%22&restrict_sr=1")).contents
+
+var parser = new DOMParser();
+var xmlDoc = parser.parseFromString(rss_content, "application/xml");
+
+var entries = xmlDoc.getElementsByTagName('entry')
+console.log(entries)
+var imageLinks = []
+Array.from(entries).forEach(entry => {
+    let entry_preview_link = entry.getElementsByTagName('media:thumbnail')[0].attributes.url.value
+    if(entry_preview_link.indexOf('://preview.') != -1){
+        imageLinks.push(entry_preview_link.split('?')[0].replace('://preview.','://i.'))
+    }
+    
+});
+console.log(imageLinks)
+
+imageLinks.forEach(image_link => {
+    var image_element = document.createElement('img')
+    image_element.src = image_link
+    image_element.onclick = (e) => {changeBackgroundCookie( e.target.src);}
+    document.getElementById('lastWallpapers').appendChild(image_element)
+})
